6e357e93dc93eb4d64a2302014b32e8ab2f0f966
zongzhili <zongzhil@nycstudents.net>
2024-11-26 14:01:40 -0500

    aa

--- app/main.js
+++ app/main.js
@@ -1,11 +1,9 @@
-import "/CSS/style.css";
+import "/style.css";
 
 async function getData() {
   //returns a promise
   try {
-    const response = await fetch(
-      "https://db.ygoprodeck.com/api/v7/cardinfo.php"
-    ); //guard clause
+    const response = await fetch("https://rickandmortyapi.com/api/character"); //guard clause
     if (response.status != 200) {
       throw new Error(response);
     } else {
@@ -13,20 +11,16 @@ async function getData() {
       const data = await response.json();
       console.log(data);
       //this is unique to THIS API
-      data.data.forEach((data) => console.log(data.name));
+      data.results.forEach((results) => console.log(results.name));
 
       const container = document.getElementById("container");
 
-      const spellCards = data.data.filter(
-        (spell) => spell.type === "Trap Card"
-      );
-
-      spellCards.forEach((data) => {
+      data.results.forEach((character) => {
         container.insertAdjacentHTML(
           "beforeend",
           `<div id = "container">
-          <img src ="${data.image_url}" alt="">
-          <h1>${data.name}</h1>
+          <img src ="${character.image}" alt="">
+          <h1>${character.name} ${character.status}</h1>
           </div>`
         );
       });
