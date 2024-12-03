4443bd49c39120584ddf12c54c7c521b2a85ba8f
zongzhili <zongzhil@nycstudents.net>
2024-12-03 14:00:00 -0500

    rcikc

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,47 +1,45 @@
 import "../CSS/style.css";
 
-async function getData() {
+async function getData(url = "https://rickandmortyapi.com/api/character") {
   //returns a promise
   try {
-    const response = await fetch("https://api.api-onepiece.com/v2/fruits/en"); //guard clause
+    const response = await fetch(url); //guard clause
 
     if (response.status !== 200) {
       throw new Error(`Error: ${response.status} ${response.statusText}`);
-    } else {
-      const data = await response.json();
-      data.forEach((fruit) => {
-        console.log(fruit.name);
-      });
     }
+    const data = await response.json();
+    console.log(data.results);
     //this is unique to THIS API
     const container = document.getElementById("container");
     container.innerHTML = ""; //clear
 
-    // data.forEach((fruit) => {
-    //   container.insertAdjacentHTML(
-    //     "beforeend",
-    //     `<div class="cards rounded-[5px] mx-auto border-2 border-black w-[24%] h-[400px] mt-[15px] mb-[15px] text-center text-black">
-    //       <h1>${fruit.name} (${fruit.roman_name})<br>Type: ${fruit.type}</h1>
-    //       <h2>${fruit.description}</h2>
-    //     </div>`
-    //   );
-    // });
+    data.results.forEach((character) => {
+      container.insertAdjacentHTML(
+        "beforeend",
+        `<div class="cards rounded-[5px] mx-auto border-2 border-black w-[24%] h-[400px] mt-[15px] mb-[15px] text-center text-black">
+          <img src ="${character.image}" class="mx-auto mt-[20px]" alt="">
+          <h1>${character.name} (${character.gender} ${character.species})</h1>
+          <h2>Status: ${character.status} on ${character.location.name}<br> Origins: ${character.origin.name}</h2>
+        </div>`
+      );
+    });
   } catch (error) {
     console.error(error);
     alert("An error occurred. Please try again.");
   }
 }
 
-// async function searchCharacter() {
-//   const searchQuery = document.getElementById("search-input").value;
-//   if (!searchQuery) return alert("Please enter a character name to search.");
+async function searchCharacter() {
+  const searchQuery = document.getElementById("search-input").value;
+  if (!searchQuery) return alert("Please enter a character name to search.");
 
-//   const searchUrl = `https://rickandmortyapi.com/api/character/?name=${searchQuery}`;
-//   getData(searchUrl);
-// }
+  const searchUrl = `https://rickandmortyapi.com/api/character/?name=${searchQuery}`;
+  getData(searchUrl);
+}
 
-// document
-//   .getElementById("search-button")
-//   .addEventListener("click", searchCharacter);
+document
+  .getElementById("search-button")
+  .addEventListener("click", searchCharacter);
 
 getData();
